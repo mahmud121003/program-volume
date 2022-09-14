a72f3bec2295016b8208aff346948a775837a909
Siapa Hayoo <113221628+mahmud121003@users.noreply.github.com>
2022-09-14 23:19:32 +0700

    program menghitung volume

---
+++ balok.py
@@ -0,0 +1,9 @@
+ #volume balok
+panjang  = int(input("masukkan panjang : "))
+lebar = int(input("masukkan lebar : "))
+tinggi = int(input("masukkan tinggi : "))
+
+volume = panjang * lebar * tinggi
+print("volume balok :" , volume)
+
+
---
+++ kubus.py
@@ -0,0 +1,7 @@
+#volume kubus
+panjang  = int(input("masukkan panjang : "))
+lebar = int(input("masukkan lebar : "))
+tinggi = int(input("masukkan tinggi : "))
+
+volume = panjang * lebar * tinggi
+print("volume kubus :" , volume)
